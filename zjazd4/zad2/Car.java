29874774e7c782ec0540aab5bc3bb46dfe3f701c
s26967-pj <115404296+s26967-pj@users.noreply.github.com>
2023-05-05 22:26:08 +0200

    Add files via upload

---
+++ zjazd4/zad2/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    protected int speed;
+    protected double regularPrice;
+    protected String color;
+
+    public Car(int speed, double regularPrice, String color) {
+        this.speed = speed;
+        this.regularPrice = regularPrice;
+        this.color = color;
+    }
+
+    public double getSalePrice() {
+        return regularPrice;
+    }
+}
---
+++ zjazd4/zad2/Sedan.java
@@ -0,0 +1,16 @@
+public class Sedan extends Car {
+    private int length;
+
+    public Sedan(int speed, double regularPrice, String color, int length) {
+        super(speed, regularPrice, color);
+        this.length = length;
+    }
+
+    public double getSalePrice() {
+        if (length > 20) {
+            return super.getSalePrice() * 0.95;
+        } else {
+            return super.getSalePrice() * 0.9;
+        }
+    }
+}
